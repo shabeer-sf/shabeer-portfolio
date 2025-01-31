d5fbf0bfe39cf926f086142588432a0d518214da
shabeer_sf <shabeermen@gmail.com>
2025-01-31 09:08:03 +0530

    first commit

--- components/Timeline.jsx
+++ components/Timeline.jsx
@@ -27,16 +27,17 @@ const Timeline = () => {
       <div className="max-w-5xl mx-auto">
         {/* Section Heading */}
         <div className="space-y-2 px-4 text-center">
-  <h2 className="text-color text-3xl md:text-4xl">Professional Journey</h2>
-  <h2 className="text-color text-3xl md:text-4xl">My Experience</h2>
-</div>
-
+          <h2 className="text-color text-3xl md:text-4xl">
+            Professional Journey
+          </h2>
+          <h2 className="text-color text-3xl md:text-4xl">My Experience</h2>
+        </div>
 
         <div className=" px-4 py-10 text-white">
           {experiences.map((exp, index) => (
             <div key={index} className="relative flex gap-6">
               {/* Year Section */}
-              <div className="text-right w-32 font-semibold text-gray-300 md:whitespace-nowrap">
+              <div className="text-right font-semibold text-gray-300 md:whitespace-nowrap">
                 {exp.year}
               </div>
 
@@ -47,7 +48,7 @@ const Timeline = () => {
 
               {/* Company & Roles */}
               <div>
-                <div className="bg-gray-700 text-white px-4 py-2 rounded-lg shadow-md text-lg font-semibold inline-block">
+                <div className="bg-[#1c1c1c] text-color px-4 py-2 rounded-lg shadow-md text-lg font-semibold inline-block">
                   {exp.company}
                 </div>
 
@@ -56,10 +57,10 @@ const Timeline = () => {
                   {exp.roles.map((role, idx) => (
                     <div
                       key={idx}
-                      className="relative flex items-center space-x-4 my-3"
+                      className="relative flex items-center gap-4 my-3"
                     >
-                      <div className="w-4 h-4 bg-blue-500 rounded-full absolute left-[-1.5rem]"></div>
-                      <div className="bg-gray-800 px-4 py-2 rounded-lg shadow-md">
+                      <div className="w-3 h-3 bg-blue-100 rounded-full absolute left-[-1.5rem]"></div>
+                      <div className="bg-[#1c1c1c] px-4 py-2 rounded-lg shadow-md">
                         {role}
                       </div>
                     </div>
